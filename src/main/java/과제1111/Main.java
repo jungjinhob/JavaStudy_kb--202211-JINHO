2c64130c0d4b8d20ab4067399ff51d19536f0107
jungjinhob <hahahyogo@gmail.com>
2022-11-17 13:15:47 +0900

    과제

---
+++ src/main/java/과제1111/Main.java
@@ -0,0 +1,19 @@
+package 과제1111;
+
+
+import java.io.BufferedReader;
+
+
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args)  {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
+
