9d0dd5c26a12b943bac0ba3a80e4511489dd2acb
syw1114 <tkdvlftj11@naver.com>
2022-11-18 12:54:48 +0900

    배열(추가)

--- src/main/java/j10_배열/Array4.java
+++ src/main/java/j10_배열/Array4.java
@@ -1,13 +1,30 @@
 package j10_배열;
 
+import java.util.Random;
+
 public class Array4 {
-    int[] nums1 = {1,2,3,4,5,6,7,8,9,10};
-    int[] nums2 = {6,7,8,9,10,11,12,13,14,15};
+
     /*
     1.두 배열에서 중복된 값 중 최소값
     2.두 배열에서 중복되지 않은 값 중 최대 값
      */
 
-    
+    public static int cmpMin(int[] array1, int[] array2){
+        int min = 0;
+        return min;
+
+    }
+    public static int notCmpMax(int[] array1, int[] array2){
+        int max = 0;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] nums2 = {6,7,8,9,10,11,12,13,14,15};
+
+
 
 }
+}
+
---
+++ src/main/java/j10_배열/Array5.java
@@ -0,0 +1,33 @@
+package j10_배열;
+
+public class Array5 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,6,7,12,13};
+        int[] nums2 = {2,7,10,11,14};
+        int[] nums3 = {3,6,11,14,19};
+        int[] nums4 = {4,5,12,13,20};
+
+        int[] resultArray = new int[5];
+        for(int i = 0 ; i< 4;i++){
+
+        }
+
+
+        //hint
+        int a = 10;
+        int b = 5;
+        int c =7;
+        int min = 0;
+        if(a<b){
+            min = a;
+        }
+        if(c<min){
+            min= c;
+        }
+    }
+
+/*
+    배열들 중 동일 인덱스에 위치한 값들 중 최소값들만 뽑아서 새로운 배열을 만든다.
+ */
+
+}
